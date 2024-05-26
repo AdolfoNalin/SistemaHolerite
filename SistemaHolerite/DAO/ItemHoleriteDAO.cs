3ed0038974ef982031ef4ceba14e289ce4e7c3e4
AdolfoNalin <nalinadolfo33@gmail.com>
2024-05-26 14:27:44 -0300

    Atributos do holerite

---
+++ SistemaHolerite/DAO/ItemHoleriteDAO.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using SistemaHolerite.Connection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaHolerite.DAO
+{
+    public class ItemHoleriteDAO
+    {
+        private MySqlConnection _connection;
+        public ItemHoleriteDAO()
+        { 
+            _connection = new ConnectionFactory().GetConnection();
+        }
+
+
+    }
+}
