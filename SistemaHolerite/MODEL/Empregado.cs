5fa9450e3df052600f9f67cf359e7a7c1bd42804
AdolfoNalin <nalinadolfo33@gmail.com>
2024-06-11 09:32:56 -0300

    Itens Empregador

---
+++ SistemaHolerite/MODEL/Empregado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaHolerite.MODEL
+{
+    public class Empregado
+    {
+        public string Nome { get; set; }
+        public string Funcao { get; set; }
+        public DateTime Mes  { get; set; }
+        public DateTime Ano { get; set; }
+    }
+}
