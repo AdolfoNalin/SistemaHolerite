4ebf6adec1c4be6ca26ee80ba0774aad2bc8f7e7
AdolfoNalin <nalinadolfo33@gmail.com>
2024-06-11 09:41:10 -0300

    Resumo geral

---
+++ SistemaHolerite/MODEL/ResumoGeral.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaHolerite.MODEL
+{
+    public class ResumoGeral
+    {
+        public float Horas { get; set; }
+        public float Extra { get; set; }
+        public float FaltaMes { get; set; }
+        public float Subtotal { get; set; }
+        public float INNS { get; set; }
+        public float OutrosDes { get; set; }
+        public float SalarioFam { get; set; }
+        public float TLR { get; set; }
+        public float VistoFicalizacao { get; set; }
+    }
+}
