27fade4e4a0ccb2915479962b319073c8f969a4d
AdolfoNalin <nalinadolfo33@gmail.com>
2024-06-11 09:40:13 -0300

    Item ponto

---
+++ SistemaHolerite/MODEL/Ponto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaHolerite.MODEL
+{
+    public class Ponto
+    {
+        public Empregado Empregado { get; set; }
+        public Empresa Empresa { get; set; }
+    }
+}
