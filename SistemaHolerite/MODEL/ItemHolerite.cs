5c78ed1e2e21eb04c8710f36ca9566672fbd678a
AdolfoNalin <nalinadolfo33@gmail.com>
2024-06-11 09:39:09 -0300

    Item do Hoolerites

--- SistemaHolerite/MODEL/ItemHolerite.cs
+++ SistemaHolerite/MODEL/ItemHolerite.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,12 @@ namespace SistemaHolerite.MODEL
 {
     public class ItemHolerite
     {
+        public int Codigo { get; set; }
+        public string Descricao{ get; set; }
+        public decimal Referencia { get; set; }
+        public float Proventos { get; set; }
+        public float Descontos { get; set; }
+        public float TotalProventos { get; set; }
+        public float TotalDesconto { get; set; }
     }
 }
