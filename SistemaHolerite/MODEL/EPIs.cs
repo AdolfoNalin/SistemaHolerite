95a842dbd8dcb61d55b9b44fbb050db9eee5f578
AdolfoNalin <nalinadolfo33@gmail.com>
2024-06-11 09:31:59 -0300

    Item de EPIS

---
+++ SistemaHolerite/MODEL/EPIs.cs
@@ -0,0 +1,15 @@
+﻿using Org.BouncyCastle.Bcpg.OpenPgp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaHolerite.MODEL
+{
+    public class EPIs
+    {
+        public string CA { get; set; }
+        public string MyProperty { get; set; }
+    }
+}
