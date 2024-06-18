0c3f00e6e235853da59d81b3b99d206bdc47f544
AdolfoNalin <nalinadolfo33@gmail.com>
2024-06-18 16:49:07 -0300

    Corrigindo erros

--- SistemaHolerite/DAO/BudgetDAO.cs
+++ SistemaHolerite/DAO/BudgetDAO.cs
@@ -22,19 +22,23 @@ namespace SistemaHolerite.DAO
             _connection = new ConnectionFactory().GetConnection();    
         }
 
-        #region insert
-        public static void Indo(Orcamento obj)
+        #region Insert
+        /// <summary>
+        /// Cadastrar o orçamento no banco de dados
+        /// </summary>
+        /// <param name="obj"></param>
+        public static void Indo(Budget obj)
         {
             try
             {
                 new BudgetDAO();
-                string sql = @"insert into tb_orcamento (id_cliente, id_fun, dp, subtotal, total
-            value (@id_client, @id_fun, @dp, @subtotal, @total";
+                string sql = @"INSERT INDO budget (cod_client, cod_emp, payment, subtotal, total
+                VALUES(@cod_client, @cod_emp, @payment, @subtotal, @total";
 
-                MySqlCommand cmd = new MySqlCommand(sql, _connection);
-                cmd.Parameters.AddWithValue("@id_client", obj.IdCliente);
-                cmd.Parameters.AddWithValue("@id_fun", obj.IdFun);
-                cmd.Parameters.AddWithValue("@dp", obj.DP);
+                MySqlCommand cmd = new MySqlCommand(sql, _connection); 
+                cmd.Parameters.AddWithValue("@cod_client", obj.CodClient);
+                cmd.Parameters.AddWithValue("@cod_emp", obj.CodEmp);
+                cmd.Parameters.AddWithValue("@payment", obj.Payment);
                 cmd.Parameters.AddWithValue("@subtotal", obj.Subtotal);
                 cmd.Parameters.AddWithValue("@total", obj.Total);
 
@@ -50,22 +54,26 @@ namespace SistemaHolerite.DAO
             {
                 _connection.Close();
             }
-            
         }
         #endregion
 
         #region UpDate
-        public static void UpDate(Orcamento obj)
+        /// <summary>
+        /// Atualizar os dados do orçamento
+        /// </summary>
+        /// <param name="obj"></param>
+        public static void UpDate(Budget obj)
         {
             try
             {
                 new BudgetDAO();
-                string sql = "update tb_orcamento (id_cliente=@id_cliente, id_fun=@id_fun, dp=@dp, subtotal=@subtotal, total=@total)";
+                string sql = "UPDATE budget(id_cliente=@id_cliente, id_fun=@id_fun, dp=@dp, subtotal=@subtotal, total=@total)";
 
                 MySqlCommand cmd = new MySqlCommand( sql, _connection);
-                cmd.Parameters.AddWithValue("@id_cliente", obj.IdCliente);
-                cmd.Parameters.AddWithValue("@id_fun", obj.IdFun);
-                cmd.Parameters.AddWithValue("@dp", obj.DP);
+                cmd.Parameters.AddWithValue("@cod_client", obj.CodClient);
+                cmd.Parameters.AddWithValue("@cod_emp", obj.CodEmp);
+                cmd.Parameters.AddWithValue("@payment", obj.Payment);
+                cmd.Parameters.AddWithValue("@date", obj.Date);
                 cmd.Parameters.AddWithValue("@subtotal", obj.Subtotal);
                 cmd.Parameters.AddWithValue("@total", obj.Total);
 
@@ -83,15 +91,19 @@ namespace SistemaHolerite.DAO
         #endregion
 
         #region Delete
-        public static void Delete(int id)
+        /// <summary>
+        /// Deleta o orçamento do banco de dados
+        /// </summary>
+        /// <param name="cod">código do orçamento</param>
+        public static void Delete(int cod)
         {
             try
             {
                 new BudgetDAO();
-                string sql = "delete tb_orcamento id where @id";
+                string sql = "DELETE budget cod WHERE @cod";
 
                 MySqlCommand cmd = new MySqlCommand(sql, _connection);
-                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@id", cod);
 
                 _connection.Open();
                 cmd.ExecuteNonQuery();
@@ -109,6 +121,10 @@ namespace SistemaHolerite.DAO
         #endregion
 
         #region Consult
+        /// <summary>
+        /// Consulta os dados do banco de dados
+        /// </summary>
+        /// <returns></returns>
         public static DataTable Consult()
         {
             try
@@ -116,7 +132,7 @@ namespace SistemaHolerite.DAO
                 new BudgetDAO();
                 DataTable dt = new DataTable();
 
-                string sql = "select * from tb_client";
+                string sql = "SELECT * FROM budget";
 
                 MySqlDataAdapter da = new MySqlDataAdapter(sql,_connection);
                 _connection.Open();
@@ -133,18 +149,35 @@ namespace SistemaHolerite.DAO
         }
         #endregion
 
-        #region ConsultName
-        public static DataTable Consult(string name)
+        #region ConsultDate
+        /// <summary>
+        /// Consulta os dados do orçamento baseado na data inicio e na data fim
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
+        public static DataTable Consult(DateTime startDate, DateTime endDate)
         {
             try
             {
                 new BudgetDAO();
                 DataTable dt = new DataTable();
 
-                string sql = "select * from tb_client name like @name";
+                string sql = @"SELECT o.id AS 'Código',
+                c.nome AS 'Cliente',
+                f.nome AS 'Vendedor',
+                b.subtotal AS 'Subtotal',
+                b.total AS 'Total',
+                b.date AS 'Data da Orçamento',
+                b.observation AS 'Observações' 
+                from budget AS b
+                JOIN client AS c ON (b.codClient = c.cod)
+                JOIN user_employee as f ON (o.cod_emp = f.cod)
+                WHERE o.date BETWEEN @startDate AND @endDate";
 
                 MySqlCommand cmd = new MySqlCommand(sql, _connection);
-                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@startDate", startDate);
+                cmd.Parameters.AddWithValue("@endDate", endDate);
 
                 _connection.Open();
                 cmd.ExecuteNonQuery();
@@ -163,45 +196,17 @@ namespace SistemaHolerite.DAO
         }
         #endregion
 
-        #region Buscar
-        public static DataTable Buscar(string name)
-        {
-            try
-            {
-                new BudgetDAO();
-                name = "%" + name + "%";
-
-                DataTable dt = new DataTable();
-
-                string sql = "select * form tb_orcamento name like @name";
-
-                MySqlCommand cmd = new MySqlCommand(sql, _connection);
-                cmd.Parameters.AddWithValue("@name", name);
-
-                _connection.Open();
-                cmd.ExecuteNonQuery();
-
-                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-                da.Fill(dt);
-
-                return dt;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Aconteceu um erro do tipo {ex.Message} com o caminho {ex.StackTrace}");
-                return null;
-            }
-            finally { _connection.Close(); }
-        }
-        #endregion
-
         #region UltimoOrcamento
+        /// <summary>
+        /// Retorna o Ultimo orçamento do banco de dados
+        /// </summary>
+        /// <returns></returns>
         public static int EndOrcamento()
         {
             int id = 0;
             try
             {
-                string sql = "Select max(id) id from tb_orcamento";
+                string sql = "SELECT MAX(cod) cod FROM budget";
 
                 MySqlCommand cmd = new MySqlCommand(sql, _connection);
 
