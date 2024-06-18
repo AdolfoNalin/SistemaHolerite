b6d5b9d3cce294e515696f6510ceaa469b0e6c90
AdolfoNalin <nalinadolfo33@gmail.com>
2024-06-18 15:02:55 -0300

    Corrigindo atributos e renomeando a classe
    Patronização

---
+++ SistemaHolerite/MODEL/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaHolerite.MODEL
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string CNPJ { get; set; }
+        public string CEP { get; set; }
+        public string State{ get; set; }
+        public string City { get; set; }
+        public string Neighborhood { get; set; }
+        public string Street { get; set; }
+        public string HomeNumber { get; set; }
+    }
+}
