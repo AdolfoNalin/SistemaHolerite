ef5ca3b1008f17b7a2d1402003e54c627dd006ff
AdolfoNalin <nalinadolfo33@gmail.com>
2024-06-18 15:24:15 -0300

    Renomeando o nome da classe e mudando os atributos

---
+++ SistemaHolerite/MODEL/Severce.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaHolerite.MODEL
+{
+    public class Severce
+    {
+        public int Cod { get; set; }
+        public string BriefDescription { get; set; }
+        public string FullDescription { get; set; }
+        public int CodEmp { get; set; }
+        public float CashPrice { get; set; }
+        public float TermPrice { get; set; }
+        public string Observation { get; set; }
+    }
+}
