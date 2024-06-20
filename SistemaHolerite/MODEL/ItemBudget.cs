15ed67e71db723c5dac13c21836210732b05d1cd
AdolfoNalin <nalinadolfo33@gmail.com>
2024-06-20 07:21:06 -0300

    Corrigindo erros

--- SistemaHolerite/MODEL/ItemBudget.cs
+++ SistemaHolerite/MODEL/ItemBudget.cs
@@ -10,7 +10,7 @@ namespace SistemaHolerite.MODEL
     {
         public int Cod { get; set; }
         public int CodBudget{ get; set; }
-        public int CodProduct{ get; set; }
+        public int CodSeverce{ get; set; }
         public float Price { get; set; }
         public int Amount{ get; set; }
         public float Subtotal { get; set; }
