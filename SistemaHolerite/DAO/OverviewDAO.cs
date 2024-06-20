f09add3a52a6e5b133c6c0d9342bc46945227547
AdolfoNalin <nalinadolfo33@gmail.com>
2024-06-20 15:01:30 -0300

    Criando a classe Overview e os métodos insert e consult
    Renomeando os nomes dos campos da classe MODEL/overview para english

---
+++ SistemaHolerite/DAO/OverviewDAO.cs
@@ -0,0 +1,100 @@
+﻿using MySql.Data.MySqlClient;
+using SistemaHolerite.Connection;
+using SistemaHolerite.MODEL;
+using SistemaHolerite.MODEL.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaHolerite.DAO
+{
+    public class OverviewDAO
+    {
+        private static MySqlConnection _connection;
+
+        public OverviewDAO()
+        {
+            _connection = new ConnectionFactory().GetConnection();
+        }
+
+        #region Insert
+        /// <summary>
+        /// Cadastra o resumo geral do ponto no banco de dados
+        /// </summary>
+        /// <param name="obj">Objeto resumo geral</param>
+        public static void Insert(Overview obj)
+        {
+            try
+            {
+                string sql = @"INSERT INTO overveiw(cod_point, time_work, extra_time, missing_month, subtotal, inss, others_des, wage_fam, trl, 
+                visa_inspection
+                VALAUE(@cod_point, @time_work, @extra_time, @missing_month, @subtotal, @inss, @others_des, @wage_fam, @trl, @visa_inspection)";
+
+                MySqlCommand cmd = new MySqlCommand(sql, _connection);
+                cmd.Parameters.AddWithValue("@cod_point", obj.CodPonto);
+                cmd.Parameters.AddWithValue("@time_work", obj.Time);
+                cmd.Parameters.AddWithValue("@extra_time", obj.MissingMoth);
+                cmd.Parameters.AddWithValue("@subtotal", obj.Subtotal);
+                cmd.Parameters.AddWithValue("@inss", obj.INSS);
+                cmd.Parameters.AddWithValue("@others_des", obj.OthersDes);
+                cmd.Parameters.AddWithValue("@wage_fam", obj.WageFam);
+                cmd.Parameters.AddWithValue("@trl", obj.TLR);
+                cmd.Parameters.AddWithValue("@visa_inspection", obj.VisaInspection);
+
+                _connection.Open();
+                cmd.ExecuteNonQuery();
+
+                Dialogo.Message("SUCESSO!", $"Resumo foi criado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Dialogo.Message("ATENÇÃO", $"Aconteceu um erro do tipo {ex.Message} com o camìnho para {ex.StackTrace}");
+                throw;
+            }
+            finally { _connection.Close(); }
+        }
+        #endregion
+
+        #region Consult
+        /// <summary>
+        /// Consulta o resumo geral baseado no código do ponto
+        /// </summary>
+        /// <param name="cod_point">código do ponto</param>
+        /// <returns></returns>
+        public static DataTable Consult(int cod_point)
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                string sql = "SELCT * FROM overview WHERE cod_point=@cod_point";
+
+                MySqlCommand cmd = new MySqlCommand( sql, _connection);
+                cmd.Parameters.AddWithValue("@cod_point", cod_point);
+
+                _connection.Open();
+                cmd.ExecuteNonQuery();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                Dialogo.Message("ATENÇÃO", $"Aconteceu um erro do tipo {ex.Message} com o caminho para {ex.StackTrace}");
+                return null;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+        #endregion
+    }
+}
--- SistemaHolerite/MODEL/Overview.cs
+++ SistemaHolerite/MODEL/Overview.cs
@@ -4,19 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SistemaHolerite.MODEL
+namespace SistemaHolerite.MODEL 
 {
     public class Overview
     {
+        public int Cod { get; set; }
         public int CodPonto { get; set; }
-        public float Horas { get; set; }
-        public float Extra { get; set; }
-        public float FaltaMes { get; set; }
+        public DateTime Time { get; set; }
+        public DateTime ExtraTime { get; set; }
+        public float MissingMoth { get; set; }
         public float Subtotal { get; set; }
         public float INSS { get; set; }
-        public float OutrosDes { get; set; }
-        public float SalarioFam { get; set; }
+        public float OthersDes { get; set; }
+        public float WageFam { get; set; }
         public float TLR { get; set; } // Total Liquido Recebido
-        public float VistoFicalizacao { get; set; }
+        public float VisaInspection { get; set; }
     }
 }
