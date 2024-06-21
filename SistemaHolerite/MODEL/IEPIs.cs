995e31bed7fa8ddaa11540a3ca05248af7dcb1e9
AdolfoNalin <nalinadolfo33@gmail.com>
2024-06-21 12:53:50 -0300

    remodelando os campos da classe e renomeando a classe e mudando para interface

---
+++ SistemaHolerite/MODEL/IEPIs.cs
@@ -0,0 +1,14 @@
+﻿using Org.BouncyCastle.Bcpg.OpenPgp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaHolerite.MODEL
+{
+    public interface IEPIs
+    {
+        public string CA { get; set; }
+    }
+}
